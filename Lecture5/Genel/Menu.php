30fdd0d1792d99fd720bf17dbb094275a24763ee
Celal Çeken <celalceken@gmail.com>
2015-10-02 15:58:05 +0300

    Lecture5

---
+++ Lecture5/Genel/Menu.php
@@ -0,0 +1,16 @@
+<div align="center">
+<?php
+
+	echo '<a href="AnaGiris.php">Ana Menu</a> | <a href="KisiselBilgiler.php">Kisisel Bilgiler</a> | <a href="SifreDegistir.php">Sifre Degistir</a> | <a href="ResimEkle.php">Resim Ekle</a>';
+	
+	// echo '<br><h4 align="center">';
+	// echo $_SESSION["no"]."<br>" ; 
+	// echo $_SESSION["ad"]." ";  
+	// echo $_SESSION['soyad'];
+	// echo "</h4>";
+// 
+  	// echo '<p align="center"><img width="56" height="72"  src=../resim/'.$_SESSION["no"].'.gif></p>';
+
+?>
+</div>
+
