61748546546c2a7bcdcb2d14e6279433247ee295
celalceken <celalceken@gmail.com>
2016-03-15 00:37:20 +0200

    PostgreSQL Configuration

--- Configuration/DatabaseConnection1.php
+++ Configuration/DatabaseConnection1.php
@@ -1,15 +1,15 @@
 <?php
 
-	$sunucu="localhost";
-	$veritabani="ob";
+	$sunucu="127.0.0.1";
+	$veritabani="OgrenciBilgiSistemi";
 	$kullaniciAdi="LectureUser";
 	$sifre="LecturePassword"; // Bu bilgileri daha güvenli bir klasörde (etc/...) saklayıp oradan erişmek daha güvenlidir.
 
-	$baglantiNo= mysqli_connect($sunucu, $kullaniciAdi, $sifre, $veritabani);
+	//$baglantiNo= pg_connect($sunucu, $kullaniciAdi, $sifre, $veritabani);
+	$baglantiNo= pg_connect("host=$sunucu dbname=$veritabani user=$kullaniciAdi password=$sifre");
+	//echo $baglantiNo;
 	// Bağlantiyi doğrula
-	if (mysqli_connect_errno()) 
+	if (pg_last_error())
 	{
-		echo "bağlantı başarısız... " . mysqli_connect_error();
+		echo "bağlantı başarısız... " . pg_last_error();
 	}
-	mysqli_set_charset($baglantiNo,"utf8");
-?>
