d72a7c3a0db10696d3408f7e5a97b2c8e7745bd9
Celal Ceken <celalceken@gmail.com>
2021-03-20 12:34:04 +0300

    Web Basics: Session Management, RBAC, Web Services

---
+++ Lecture4/3.2.RestClientWithWebApp/Server.js
@@ -0,0 +1,13 @@
+
+
+//Dependencies
+
+var express = require('express'); //Node.js web uygulama çatısı. Web uygulamaları geliştirmek için kullanılır.
+var app = express();
+var path = require('path');
+
+// Statik içeriklerin (resim, css, js vb.) istemcilere gönderilebilmesi için yol tanımı yapılıyor. express.static fonksiyonu(express çerisinde tanımlı) fonksiyonu kullanılıyor.
+app.use("/",express.static(path.join(__dirname, 'public'))); // (virtual path prefix)ana klasördeki public klasörü kök dizin olarak tanımlanır
+app.listen(8001);
+console.log('8001 Portu dinleniyor...');
+
